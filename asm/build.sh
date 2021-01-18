40903ccb2a658c84a7882e02985b3b8ac6528145
Zander Work <zander@zanderwork.com>
2021-01-18 00:00:10 -0800

    added untested intercept shellcode

--- asm/build.sh
+++ asm/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 for asm_file in $(ls *.S); do
+    echo "[~] building $asm_file"
+    # https://stackoverflow.com/a/965072
     base="${asm_file%.*}"
-    gcc -o $base.exe -m64 $asm_file
+
+    # build object file and extract .text
+    gcc -m64 -c -o $base.o $asm_file && \
+    objcopy -S -O binary -j .text $base.o $base.bin
 done
+
+echo "[~] done"
