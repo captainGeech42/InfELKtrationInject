550a0e6b755eae944c5c99ad2d2947403787b2a8
Zander Work <zander@zanderwork.com>
2021-01-18 13:37:33 -0800

    basic injection + logging works, heck yis

--- InfELKtrationInjectLib/HttpIntercept.cpp
+++ InfELKtrationInjectLib/HttpIntercept.cpp
@@ -2,9 +2,9 @@
 #include "HttpIntercept.h"
 
 void HttpIntercept(char* reqBytes, char* respBytes, char* apiKey) {
-	// Logger::Info("Executing HTTP interception code");
+	Logger::Info("Executing HTTP interception code");
 
-	// Logger::Info("API Key: %s", apiKey);
+	Logger::Info("API Key: %s", apiKey);
 
 	int a = 3;
 	a = 3 + 7;
